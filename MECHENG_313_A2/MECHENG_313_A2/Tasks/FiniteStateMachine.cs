904b8982fb50179521f77cbd613f798177c1bca8
Cole Parker <coleashtonparker@gmail.com>
2023-05-23 19:57:46 +1200

    Add project files.

---
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MECHENG_313_A2.Tasks
+{
+
+    //public delegate int DelFoo(int a, int b);
+
+    public class FiniteStateMachine : IFiniteStateMachine
+    {
+
+        public struct Info {
+            public string nextState;
+            public List<TimestampedAction> actions;
+        }
+
+        public Dictionary<string, Dictionary<string, Info>> fst = new Dictionary<string, Dictionary<string, Info>>();
+        private string currentState;
+
+        public void AddAction(string state, string eventTrigger, TimestampedAction action)
+        {
+
+            Info a = fst[state][eventTrigger];
+            Info b;
+            b = a;
+            b.actions.Add(action);
+            fst[state][eventTrigger] = b;
+
+        }
+
+        public string GetCurrentState()
+
+        {
+            return currentState;
+        }
+
+        public string ProcessEvent(string eventTrigger)
+        {
+            //DelFoo foo = (a, b) => { return a + b; };
+            //foo(1, 2);
+
+            //DateTime time = DateTime.Now;
+
+            if (fst.ContainsKey(currentState) && fst[currentState].ContainsKey(eventTrigger))
+            {
+                TimestampedAction temp;
+                Info info = fst[currentState][eventTrigger];
+                SetCurrentState(info.nextState);
+
+                for (var i = 0; i < info.actions.Count; i++)
+                {
+                    temp = info.actions[i];
+                    temp(DateTime.Now, eventTrigger);
+                }
+
+                SetCurrentState(info.nextState);
+                return info.nextState;
+            }
+
+            return currentState;
+
+        }
+
+        public void SetCurrentState(string state)
+        {
+
+            currentState = state;
+
+        }
+
+        public void SetNextState(string state, string nextState, string eventTrigger)
+        {
+            Info info;
+            info.nextState = nextState;
+            info.actions = new List<TimestampedAction>();
+            if (!fst.ContainsKey(state))
+            {
+                fst.Add(state, new Dictionary<string, Info>());
+            }
+            if (!fst[state].ContainsKey(eventTrigger))
+            {
+               fst[state].Add(eventTrigger, info);
+            }
+            
+
+        }
+
+    }
+}
---
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/Task2.cs
@@ -0,0 +1,171 @@
+﻿using MECHENG_313_A2.Serial;
+using MECHENG_313_A2.Views;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECHENG_313_A2.Tasks
+{
+    internal class Task2 : IController
+    {
+
+        private string state = null;
+        private string tempState = null;
+        private FiniteStateMachine FSM = new FiniteStateMachine();
+        private MockSerialInterface serialInterface = new MockSerialInterface();
+        TrafficLightState displayState = new TrafficLightState();
+        public virtual TaskNumber TaskNumber => TaskNumber.Task2;
+
+        protected ITaskPage _taskPage;
+
+        public void ConfigLightLength(int redLength, int greenLength)
+        {
+            // TODO: Implement this
+        }
+
+        public async Task<bool> EnterConfigMode()
+        {
+            //Can only enter config mode is the current state is Red
+            if (String.Equals(FSM.GetCurrentState(), "Red"))
+            {
+                //Using ProcessEvent method to enter config mode. use of state variable is only since Process Event returns current state
+                state = FSM.ProcessEvent("b");
+                return true;
+            }
+            return false;
+        }
+
+        public void ExitConfigMode()
+        {
+            //Using ProcessEvent method to exit config mode. use of state variable is only since Process Event returns current state
+            state = FSM.ProcessEvent("b");
+        }
+
+        public async Task<string[]> GetPortNames()
+        {
+            //Using the MockSerialInterface class to obtain port names (.Result converts from Task <string[]> to string[])
+            return serialInterface.GetPortNames().Result;
+        }
+
+        public async Task<string> OpenLogFile()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "log.txt");
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+
+            }
+            else
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                _taskPage.SetLogEntries(lines);
+            }
+
+            
+            
+
+            // Help notes: to read a file named "log.txt" under the LocalApplicationData directory,
+            // you may use the following code snippet:
+            // string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "log.txt");
+            // string text = File.ReadAllText(filePath);
+            //
+            // You can also create/write to file(s) through System.IO.File. 
+            // See https://learn.microsoft.com/en-us/xamarin/xamarin-forms/data-cloud/data/files?tabs=windows, and
+            // https://learn.microsoft.com/en-us/dotnet/api/system.io.file?view=netstandard-2.0 for more details.
+            return filePath;
+        }
+
+        public async Task<bool> OpenPort(string serialPort, int baudRate)
+        {
+            bool open = await serialInterface.OpenPort(serialPort, baudRate).ConfigureAwait(false);
+            if (open)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void RegisterTaskPage(ITaskPage taskPage)
+        {
+            _taskPage = taskPage;
+        }
+
+       
+
+        public void Start()
+        {
+            //Setting the initial Green state
+            state = "Green";
+            FSM.SetCurrentState(state);
+            FSM.SetNextState(state, "Yellow", "a");
+            FSM.AddAction(state, "a", MethodA);
+            FSM.AddAction(state, "a", MethodB);
+            FSM.AddAction(state,"a", MethodC);
+
+            //Setting all other states
+            FSM.SetNextState("Yellow", "Red", "a");
+            FSM.AddAction("Yellow", "a", MethodA);
+            FSM.AddAction("Yellow", "a", MethodB);
+            FSM.AddAction("Yellow", "a", MethodC);
+
+            FSM.SetNextState("Red", "Green", "a");
+            FSM.AddAction("Red", "a", MethodA);
+            FSM.AddAction("Red", "a", MethodB);
+            FSM.AddAction("Red", "a", MethodC);
+
+            FSM.SetNextState("Red", "Yellow'", "b");
+            FSM.AddAction("Red", "b", MethodA);
+            FSM.AddAction("Red", "b", MethodB);
+            FSM.AddAction("Red", "b", MethodC);
+
+            FSM.SetNextState("Yellow", "Red", "b");
+            FSM.AddAction("Yellow", "b", MethodA);
+            FSM.AddAction("Yellow", "b", MethodB);
+            FSM.AddAction("Yellow", "b", MethodC);
+
+            FSM.SetNextState("None", "Red", "b");
+            FSM.AddAction("None", "b", MethodA);
+            FSM.AddAction("None", "b", MethodB);
+            FSM.AddAction("None","b", MethodC);
+
+            FSM.SetNextState("Yellow", "None", "a");
+            FSM.AddAction("Yellow", "a", MethodA);
+            FSM.AddAction("Yellow", "a", MethodB);
+            FSM.AddAction("Yellow", "a", MethodC);
+
+            FSM.SetNextState("None", "Yellow", "a");
+            FSM.AddAction("None", "a", MethodA);
+            FSM.AddAction("None", "a", MethodB);
+            FSM.AddAction("None", "a", MethodC);
+        }
+
+        public void Tick()
+        {
+            state = FSM.ProcessEvent("a");
+
+        }
+
+        public void MethodA(DateTime timestamp, string eventTrigger)
+        {
+            string temp = FSM.GetCurrentState();
+            if (Enum.TryParse<TrafficLightState>(temp, out displayState))
+            {
+                _taskPage.SetTrafficLightState((TrafficLightState)Enum.Parse(typeof(TrafficLightState), temp, true));
+            }
+        }
+
+        public void MethodB(DateTime timestamp, string eventTrigger)
+        {
+            string logEntry = DateTime.Now + eventTrigger + "\n";
+            _taskPage.AddLogEntry(logEntry);
+        }
+
+        public void MethodC(DateTime timestamp, string eventTrigger)
+        {
+         
+            _taskPage.SerialPrint(DateTime.Now, FSM.GetCurrentState());
+        }
+    }
+}
